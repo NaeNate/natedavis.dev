7f1e1a79b5373c86b40dc11c0d65e85e4cc2c02b
Nate D <nate@qstreet.org>
2022-09-18 15:19:15 +0300

    First Commit

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next"
+
+const About: NextPage = () => {
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        Hi. I'm Nate Davis. I'm a 17 year old developer who works mainly with
+        javascript and rust. I'm setting this website up because I'm bored and I
+        need to learn Next.js better.
+      </p>
+    </>
+  )
+}
+
+export default About
